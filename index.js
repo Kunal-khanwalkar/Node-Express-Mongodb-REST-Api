a70d699a9db76f2564eb7763d13d87b12fd8135f
Kunal-khanwalkar <kunalkhanwalkar@gmail.com>
2020-09-01 16:01:43 +0530

    Added driver code

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const multer = require('multer');
+require('dotenv/config');
+
+app.use(express.json());
+
+//Router
+const routes = require('./controllers/router');
+app.use('/',routes);
+
+app.use(multer({
+	dest: './uploads/',
+	rename: (fieldname,filename) => {
+		return filename;
+	},
+}).single('photo'));
+
+
+//Database Connection
+mongoose.connect(process.env.DB_CONNECTION, { useNewUrlParser: true, useUnifiedTopology: true }, () => 
+	console.log('Connected to Database!')
+);
+
+
+//Server start
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Listening to port ${port}...`));
